08b321d851b073910272d0d50fe5c53d1197da42
fyw4 <wangqingchuan92@126.com>
2026-04-07 17:03:08 +0800

    轮廓检测功能

--- license_plate_detect_demo.py
+++ license_plate_detect_demo.py
@@ -29,6 +29,22 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 第四个参数：坐标空间的标准差
 '''
 # 正确的双边滤波调用
-filtered = cv2.bilateralFilter(gray, 9, 75, 75)
+filtered = cv2.bilateralFilter(gray, 13, 15, 15)
 
-show_image("gray", filtered)
+# show_image("gray", filtered)
+
+# 边缘检测 canny边缘检测 第一个参数：输入图像 第二个参数：低阈值 第三个参数：高阈值
+edges = cv2.Canny(filtered, 30, 200)
+
+# show_image("edges", edges)
+
+# 寻找轮廓 CHAIN_APPROX_SIMPLE保留轮廓的端点信息
+contours, _ = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+# 找到前10个contourArea轮廓面积和排序
+contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+
+# 画轮廓
+cv2.drawContours(img, contours, -1, [0, 255, 0])
+
+show_image("img", img)
